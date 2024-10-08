de1c62baa3e3396744c240d48ad188da68111f84
15cf0408 <tom.lemarchandcholot@gmail.com>
2024-10-08 16:17:51 +0200

    Adding images + css + html files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 const apiUrlGeo = 'https://geo.api.gouv.fr/communes?codePostal=';
 const token = 'b05f0cb126dfe4edd5d71b72ee1d429986f8569870242d37e56f6f7d6a199e10';
 const apiUrlMeteo = 'https://api.meteo-concept.com/api/forecast/daily?token='+ token +'&insee=';
@@ -59,18 +58,38 @@ getWeatherButton.addEventListener('click', async () => {
         const data = await response.json();
         console.log(data);
 
-        const weatherInfoDiv = document.getElementById('weatherInfo');
-        weatherInfoDiv.innerHTML = `
-            <h3>Météo pour ${communeSelect.options[communeSelect.selectedIndex].text}</h3>
-            <p>Température : ${data.forecast[0].tmin}°C - ${data.forecast[0].tmax}°C</p>
-            <p>Probabilité de pluie : ${data.forecast[0].probarain}%</p>
-            <p>Heure d'ensoleillements : ${data.forecast[0].sun_hours}heures</p>
-            <p>Prévisions : ${data.forecast[0].weather}</p>
+        // const weatherInfoDiv = document.getElementById('weatherInfo');
+        // //weatherInfoDiv.innerHTML = `
+        // //    <h3>Météo pour ${communeSelect.options[communeSelect.selectedIndex].text}</h3>
+        //  //   <div class="num">${data.forecast[0].tmin}°C - ${data.forecast[0].tmax}°C</div>
+        //    // <p>Prévisions : ${data.forecast[0].weather}</p>
+        //     //<p>Heure d'ensoleillements : ${data.forecast[0].sun_hours}heures</p>
+        //     //<p>Prévisions : ${data.forecast[0].weather}</p>
+        // //`;
+
+        const locationId = document.getElementById('locationId');
+        locationId.innerHTML = `
+            ${communeSelect.options[communeSelect.selectedIndex].text}
+        `;
+
+        const tempID= document.getElementById('tempID');
+        tempID.innerHTML = `
+           <div class="num">Max:${data.forecast[0].tmin}<sup>o</sup>C 
+           <div class="num">Min:${data.forecast[0].tmax}<sup>o</sup>C
+        `;
+
+        const precipitationID = document.getElementById('precipitationID');
+        precipitationID.innerHTML = `
+            <img src="images/icons8-parapluie-48.png" alt="" width="30">
+            ${data.forecast[0].probarain}%
+        `;
+
+        const soleilID = document.getElementById('soleilID');
+        soleilID.innerHTML = `
+            <img src="images/icons/icons8-soleil-48.png" alt="" width="30">
+            ${data.forecast[0].sun_hours}h
         `;
     } catch(err) {
         console.log("Erreur lors de l'envoie de la requête à l'API : " + err);
     }
 })
-
-
-
