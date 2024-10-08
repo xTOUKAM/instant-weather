aa03d987a525d1930fd95af73131c5e7e1ffcd80
Tom HOULLEGATTE <houllegatte.tom@gmail.com>
2024-10-08 15:02:36 +0200

    Adding sun hours

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const apiUrlGeo = 'https://geo.api.gouv.fr/communes?codePostal=';
 const token = 'b05f0cb126dfe4edd5d71b72ee1d429986f8569870242d37e56f6f7d6a199e10';
 const apiUrlMeteo = 'https://api.meteo-concept.com/api/forecast/daily?token='+ token +'&insee=';
 const communeSelect = document.getElementById('communeSelect');
+const getWeatherButton = document.getElementById('getWeatherButton');
 let postalCode = document.getElementById('postalCode');
 
 async function getCommuneByCP(postalCode) {
@@ -21,6 +22,7 @@ function displayCommunes(data) {
     communeSelect.innerHTML = "";
     if(data.length >= 1) {
         communeSelect.style.display = 'block';
+        getWeatherButton.style.display = 'block';
         data.forEach(commune => {
             let addingCommune = document.createElement('option');
                 addingCommune.value = commune.code;
@@ -32,6 +34,7 @@ function displayCommunes(data) {
     if(data.length === 0) {
         postalCode.value = "";
         communeSelect.style.display = 'none';
+        getWeatherButton.style.display = 'none';
     }
 }
 
@@ -48,8 +51,6 @@ postalCode.addEventListener("input", async () => {
     }
 })
 
-const getWeatherButton = document.getElementById('getWeatherButton');
-
 getWeatherButton.addEventListener('click', async () => {
     let communeValue = communeSelect.value;
     let getUrlForMeteo = apiUrlMeteo+communeValue;
@@ -62,6 +63,8 @@ getWeatherButton.addEventListener('click', async () => {
         weatherInfoDiv.innerHTML = `
             <h3>Météo pour ${communeSelect.options[communeSelect.selectedIndex].text}</h3>
             <p>Température : ${data.forecast[0].tmin}°C - ${data.forecast[0].tmax}°C</p>
+            <p>Probabilité de pluie : ${data.forecast[0].probarain}%</p>
+            <p>Heure d'ensoleillements : ${data.forecast[0].sun_hours}heures</p>
             <p>Prévisions : ${data.forecast[0].weather}</p>
         `;
     } catch(err) {
